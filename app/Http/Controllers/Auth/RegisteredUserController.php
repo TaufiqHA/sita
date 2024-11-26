2944ce8352e5fcc269c9852d208d4c744d9df609
taufiq <htaufiq225@gmail.com>
2024-11-26 20:14:50 +0800

    instalasi project

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Models\Mahasiswa;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Auth\Events\Registered;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'nama' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.Mahasiswa::class],
+            'nim' => ['string', 'max:255'],
+            'alamat' => ['string', 'max:255'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = Mahasiswa::create([
+            'nama' => $request->nama,
+            'email' => $request->email,
+            'nim' => $request->nim,
+            'alamat' => $request->alamat,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(route('dashboard', absolute: false));
+    }
+}
---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Mahasiswa extends Authenticatable
+{
+    use Notifiable;
+    protected $fillable = [
+        'nama', 'email', 'nim', 'alamat', 'password', // Kolom-kolom di tabel mahasiswa
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+}
