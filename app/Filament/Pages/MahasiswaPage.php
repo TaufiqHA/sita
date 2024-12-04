dfcf0430e2d11e0520fc875d3514c0e8072aa0b5
taufiq <htaufiq225@gmail.com>
2024-12-04 21:42:13 +0800

    menambahkan halaman monitor progress mahasiswa untuk kajur

--- app/Filament/Pages/MahasiswaPage.php
+++ app/Filament/Pages/MahasiswaPage.php
@@ -3,10 +3,59 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use App\Models\Mahasiswa;
+use Filament\Tables\Table;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Concerns\InteractsWithTable;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\TextColumn;
 
-class MahasiswaPage extends Page
+class MahasiswaPage extends Page implements HasTable
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    use HasPageShield, InteractsWithTable;
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
 
     protected static string $view = 'filament.pages.mahasiswa-page';
+
+    protected static ?string $navigationLabel = 'Mahasiswa';
+
+    protected static ?string $navigationGroup = 'Bimbingan';
+
+    public function table(Table $table): Table {
+        return $table
+            ->query(Mahasiswa::query())
+            ->columns([
+                Split::make([
+                    TextColumn::make('name')
+                        ->searchable(),
+                    TextColumn::make('nim')
+                        ->searchable(),
+                    Stack::make([
+                        TextColumn::make('pembimbing.dospem1.name'),
+                        TextColumn::make('pembimbing.status_dospem1')
+                            ->label('Pembimbing 1')
+                            ->badge()
+                            ->color(fn ($state) => match ($state) {
+                                'bimbingan' => 'warning',
+                                'diterima' => 'success',
+                                'ditolak' => 'danger'
+                            }),
+                        ]),
+                    Stack::make([
+                        TextColumn::make('pembimbing.dospem2.name'),
+                        TextColumn::make('pembimbing.status_dospem2')
+                            ->label('Pembimbing 2')
+                            ->badge()
+                            ->color(fn ($state) => match ($state) {
+                                'bimbingan' => 'warning',
+                                'diterima' => 'success',
+                                'ditolak' => 'danger'
+                            }),
+                        ]),
+                ])
+            ]);
+    }
+
 }
--- app/Models/Pembimbing.php
+++ app/Models/Pembimbing.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pembimbing extends Model
 {
     protected $guarded = ['id'];
+
+    public function dospem1(): BelongsTo {
+        return $this->belongsTo(Dosen::class, 'dospem1_id');
+    }
+
+    public function dospem2(): BelongsTo {
+        return $this->belongsTo(Dosen::class, 'dospem2_id');
+    }
 }
