8c81a294b099ded9b812502167edf09d085ffeec
taufiq <htaufiq225@gmail.com>
2025-02-11 21:23:34 +0800

    Implementasi autentikasi dan manajemen pengguna (Login, Registrasi, Role Management)

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +49,22 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'admin') {
+            return Auth::user()->roles->contains('name', 'super_admin');
+        }elseif ($panel->getId() === 'mahasiswa') {
+            return Auth::user()->roles->contains('name', 'mahasiswa');
+        }elseif ($panel->getId() === 'kajur') {
+            return Auth::user()->roles->contains('name', 'kajur');
+        }elseif ($panel->getId() === 'sekjur') {
+            return Auth::user()->roles->contains('name', 'sekjur');
+        }elseif ($panel->getId() === 'dosen') {
+            return Auth::user()->roles->contains('name', 'dosen');
+        }
+
+        return true;
     }
 }
